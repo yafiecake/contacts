cfdf175f45766476d3fbc0736ab53dcb972e2db4
Mark Warren Lalata <markwarren.dev@gmail.com>
2019-06-03 18:20:48 +0800

    Add prod comment

--- db/models/index.js
+++ db/models/index.js
@@ -14,6 +14,7 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 }
 else if (process.env.DATABASE_URL) {
+  console.log('using the production database_url')
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
